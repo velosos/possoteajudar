b29228c22b5fded72c3b4d3f7d3311d18958490c
thiago.veloso <thiago.veloso@corp.globo.com>
2017-08-08 11:06:05 -0300

    all

--- getKbs/settings.py
+++ getKbs/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'bfa9noi9z*!)&bk7um#5l*j5k@uq15qthoibe!iu+4vsm-03se'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1','.cloud.globoi.com']
+
+
 
 
 # Application definition
